da1976620918db574c928508dbdad204cea25f24
AT <athinat690@gmail.com>
2024-05-22 22:13:43 +0100

    profile ui

--- app/(tabs)/profile.jsx
+++ app/(tabs)/profile.jsx
@@ -3,27 +3,34 @@ import React from "react";
 import { signOut } from "../../api/appwrite";
 import { router } from "expo-router";
 import useGlobalContext from "../../context/GlobalProvider";
+import ProfileNav from "../../components/ProfileNav";
+import { SafeAreaView } from "react-native-safe-area-context";
+import ProfileHeader from "../../components/ProfileHeader";
 
 const Profile = () => {
   // const { setUser, setIsLogged } = useGlobalContext();
 
   const logout = async () => {
     await signOut();
-    // setUser(null);
-    // setIsLogged(false);
+    setUser(null);
+    setIsLogged(false);
 
     router.replace("/sign-in");
   };
 
   return (
-    <View className="h-full w-full items-center justify-center">
-      <TouchableOpacity
-        onPress={logout}
-        className="flex w-full items-end mb-10"
-      >
-        <Text>SignOut</Text>
-      </TouchableOpacity>
-    </View>
+    <SafeAreaView className="h-full w-full bg-white">
+      <View className="px-2 h-screen justify-center items-center flex-col">
+        <ProfileNav />
+        <ProfileHeader />
+        <TouchableOpacity
+          onPress={logout}
+          className="flex w-full items-end mb-10"
+        >
+          <Text>SignOut</Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   );
 };
 
---
+++ components/ProfileHeader.jsx
@@ -0,0 +1,48 @@
+import { View, Text, Image } from "react-native";
+import React from "react";
+
+const ProfileHeader = () => {
+  return (
+    <View className="w-full mt-10 ">
+      <View className="flex-row items-center justify-start">
+        <View className="w-[105px] h-[105px] bg-pink-500 rounded-full items-center justify-center">
+          <View className="bg-[#DCDEE0] h-[100px] w-[100px] rounded-full" />
+        </View>
+        <View className="flex-col pl-5 gap-2">
+          <Text className="font-pbold text-[19px]">Musiani Wanda</Text>
+          <Text className="font-pregular text-[13px] text-[#81999E]">
+            Designer{" "}
+            <Text className="font-pregular text-[13px] text-black">@Pipes</Text>
+          </Text>
+          <Text className="font-pregular text-[13px] text-black">
+            Whatever this is!
+          </Text>
+        </View>
+      </View>
+      <View className="flex-row gap-2 w-full items-center justify-around mt-4">
+        <View className="flex-col">
+          <Text className="text-[#52697E] text-[14px] font-pregular">
+            Posts
+          </Text>
+          <Text className="font-psemibold text-[20px]">1,324</Text>
+        </View>
+        <View className="h-[50px] w-[2px] bg-[#DCDEE0]" />
+        <View className="flex-col">
+          <Text className="text-[#52697E] text-[14px] font-pregular">
+            Followers
+          </Text>
+          <Text className="font-psemibold text-[20px]">2.3m</Text>
+        </View>
+        <View className="h-[50px] w-[2px] bg-[#DCDEE0]" />
+        <View className="flex-col">
+          <Text className="text-[#52697E] text-[14px] font-pregular">
+            Friends
+          </Text>
+          <Text className="font-psemibold text-[20px]">2,523</Text>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export default ProfileHeader;
